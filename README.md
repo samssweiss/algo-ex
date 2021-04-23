373ae19e610a64c0f4f08213b323e61006012682
samssweiss <samssweiss@gmail.com>
2021-04-23 15:09:47 +0800

    feat: two sum

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # algo-ex
 algo-ex
+
+
+# leetcode | 1 two sum
+- https://leetcode-cn.com/problems/two-sum
+- https://github.com/azl397985856/leetcode/blob/master/problems/1.two-sum.md
+- https://github.com/azl397985856/leetcode/blob/master/problems/1.two-sum.en.md
---
+++ leetcode-top100/1-two-sum.js
@@ -0,0 +1,27 @@
+/**
+ * 
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那 两个 整数，并返回它们的数组下标。
+
+ 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+
+ 你可以按任意顺序返回答案。
+ * 
+ */
+
+ 
+ /**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = function (nums, target) {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const diff = target - nums[i];
+    if (map.has(diff)) {
+      return [map.get(diff), i];
+    }
+    map.set(nums[i], i);
+  }
+};
+
