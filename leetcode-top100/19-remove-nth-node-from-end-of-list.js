f1f80ea73fe31b5539747dc63337ecf23cc33bc5
samssweiss <samssweiss@gmail.com>
2021-04-25 20:25:54 +0800

    update

--- leetcode-top100/19-remove-nth-node-from-end-of-list.js
+++ leetcode-top100/19-remove-nth-node-from-end-of-list.js
@@ -47,4 +47,29 @@ var removeNthFromEnd = function (head, n) {
   return dummyHead.next;
 };
 
-console.log(removeNthFromEnd([1, 2, 3, 4, 5], 2));
+
+function createNodeList(arr) {
+  // function ListNode(val, next) {
+  //   this.val = val === undefined ? 0 : val;
+  //   this.next = next === undefined ? null : next;
+  // }
+
+  let head = null
+  let tail = null
+  
+  const n = arr.length
+  for (let index = 0; index < n; index++) {
+    if (index === 0) {
+      head = new ListNode(arr[index])
+      tail = head
+    } else {
+      const temp = new ListNode(arr[index])
+      tail.next = temp
+      tail = temp
+    }
+  }
+  
+  return head
+}
+
+console.log(removeNthFromEnd(createNodeList([1, 2, 3, 4, 5]), 2));
--- utils/utils.js
+++ utils/utils.js
@@ -4,20 +4,20 @@ function createNodeList(arr) {
     this.next = next === undefined ? null : next;
   }
 
-  const head = null
-  const tail = null
-  
-  const n = arr.length
+  let head = null;
+  let tail = null;
+
+  const n = arr.length;
   for (let index = 0; index < n; index++) {
     if (index === 0) {
-      head = new ListNode(val)
-      tail = head
+      head = new ListNode(arr[index]);
+      tail = head;
     } else {
-      const temp = new ListNode(val)
-      tail.next = temp
-      tail = temp
+      const temp = new ListNode(arr[index]);
+      tail.next = temp;
+      tail = temp;
     }
   }
-  
-  return head
+
+  return head;
 }
